d6d1ea4966bdc8604ada865111b39c4c0be06739
Roberta Parcianello <roberta.parcianello@ifpr.edu.br>
2025-10-20 08:22:11 -0300

    findById, save, @PathVariable e @RequestBody

--- src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -3,11 +3,17 @@ package br.edu.ifpr.cars.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.edu.ifpr.cars.domain.Driver;
 import br.edu.ifpr.cars.domain.DriverRepository;
@@ -25,4 +31,25 @@ public class DriverController {
         return driverRepository.findAll();
     }
 
+    // definir uma Exception personalizada
+    @GetMapping("/drivers/{id}")
+    public Driver findDriver(@PathVariable("id") Long id) {
+        return driverRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/drivers")
+    public Driver createDriver(@RequestBody Driver driver) {
+        return driverRepository.save(driver);
+    }
+
+    // update
+    @PutMapping("/drivers{id}")
+    public Driver fullUpdateDriver(@PathVariable("id") Long id,
+            @RequestBody Driver driver) {
+        Driver foundDriver = findDriver(id);
+        foundDriver.setName(driver.getName());
+        foundDriver.setBirthDate(driver.getBirthDate());
+        return driverRepository.save(foundDriver);
+    }
 }
