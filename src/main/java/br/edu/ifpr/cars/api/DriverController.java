e6e143189cdb52941fab71047887706cbc065747
Roberta Parcianello <roberta.parcianello@ifpr.edu.br>
2025-10-21 14:12:40 -0300

    put patch e delete

--- src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -1,12 +1,15 @@
 package br.edu.ifpr.cars.api;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -44,7 +47,7 @@ public class DriverController {
     }
 
     // update
-    @PutMapping("/drivers{id}")
+    @PutMapping("/drivers/{id}")
     public Driver fullUpdateDriver(@PathVariable("id") Long id,
             @RequestBody Driver driver) {
         Driver foundDriver = findDriver(id);
@@ -52,4 +55,25 @@ public class DriverController {
         foundDriver.setBirthDate(driver.getBirthDate());
         return driverRepository.save(foundDriver);
     }
+
+    @PatchMapping("/drivers/{id}")
+    public Driver incrementalUpdateDriver(@PathVariable("id") Long id,
+            @RequestBody Driver driver){
+            Driver foundDriver = findDriver(id);
+            
+            foundDriver.setName(Optional.ofNullable(driver.getName())
+            .orElse(foundDriver.getName()));
+
+            foundDriver.setBirthDate(Optional.ofNullable(driver.getBirthDate())
+            .orElse(foundDriver.getBirthDate()));
+
+            return driverRepository.save(foundDriver);
+    }
+
+    @DeleteMapping("/drivers/{id}")
+    public void deleteDriver(@PathVariable("id") Long id){
+        driverRepository.deleteById(id);
+    }
+
+
 }
